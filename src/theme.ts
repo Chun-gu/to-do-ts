aa349a11cce1dc0158aebf0444361d3dbb02b6bc
Chun-gu <chungu.yi92@gmail.com>
2022-02-06 03:12:33 +0900

    💄 theme 작성

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#222831",
+  textColor: "#EEEEEE",
+  contentColor: "#393E46",
+  accentColor: "#00ADB5",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#FFFFFF",
+  textColor: "#222831",
+  contentColor: "#EEEEEE",
+  accentColor: "#00ADB5",
+};

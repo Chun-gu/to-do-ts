e4c556bcc137588c69199428295cec1e14bbdfb5
Chun-gu <chungu.yi92@gmail.com>
2022-02-15 23:34:06 +0900

    ✨ selector에 따른 todos 분리 구현

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,16 +1,31 @@
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import { useRecoilValue } from "recoil";
 import ToDoItem from "./ToDoItem";
 
 const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
+  const [toDos, doings, dones] = useRecoilValue(toDoSelector);
 
   return (
-    <ul>
-      {toDos.map((toDo) => (
-        <ToDoItem key={toDo.id} {...toDo} />
-      ))}
-    </ul>
+    <>
+      <h2>To Do</h2>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDoItem key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <h2>Doing</h2>
+      <ul>
+        {doings.map((toDo) => (
+          <ToDoItem key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <h2>Done</h2>
+      <ul>
+        {dones.map((toDo) => (
+          <ToDoItem key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+    </>
   );
 };
 

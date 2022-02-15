934e3fa257cd3fb9d601fa242fab1d2865628f05
Chun-gu <chungu.yi92@gmail.com>
2022-02-15 23:55:41 +0900

    ✨ category 선택에 따른 선택적 렌더링

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,30 +1,15 @@
-import { toDoSelector, toDoState } from "../atoms";
+import { toDoSelector } from "../atoms";
 import { useRecoilValue } from "recoil";
 import ToDoItem from "./ToDoItem";
 
 const ToDoList = () => {
-  const [toDos, doings, dones] = useRecoilValue(toDoSelector);
+  const toDos = useRecoilValue(toDoSelector);
 
   return (
     <>
-      <h2>To Do</h2>
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDoItem key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <h2>Doing</h2>
-      <ul>
-        {doings.map((toDo) => (
-          <ToDoItem key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <h2>Done</h2>
-      <ul>
-        {dones.map((toDo) => (
-          <ToDoItem key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDoItem key={toDo.id} {...toDo} />
+      ))}
     </>
   );
 };

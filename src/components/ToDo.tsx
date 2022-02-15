3e672be8d28564cbd7ebe92a4348479a638ef582
Chun-gu <chungu.yi92@gmail.com>
2022-02-15 23:55:15 +0900

    ✨ todo 목록 구분하는 category 생성

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,23 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { categoryState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDoList from "./ToDoList";
 
 const ToDo = () => {
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <>
       <h1>To-dos</h1>
       <CreateToDo />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <ToDoList />
     </>
   );

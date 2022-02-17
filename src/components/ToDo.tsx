cd7153d8b9907cfe6d28cc51d1e3665b0f51a49d
Chun-gu <chungu.yi92@gmail.com>
2022-02-17 23:46:10 +0900

    ♻️ category selection 분리

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,23 +1,13 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { Categories, categoryState } from "../atoms";
+import CategorySelection from "./CategorySelection";
 import CreateToDo from "./CreateToDo";
 import ToDoList from "./ToDoList";
 
 const ToDo = () => {
-  const [category, setCategory] = useRecoilState(categoryState);
-  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value as any);
-  };
   return (
     <>
       <h1>To-dos</h1>
       <CreateToDo />
-      <select value={category} onInput={onInput}>
-        <option value={Categories.TO_DO}>To Do</option>
-        <option value={Categories.DOING}>Doing</option>
-        <option value={Categories.DONE}>Done</option>
-      </select>
+      <CategorySelection />
       <ToDoList />
     </>
   );
